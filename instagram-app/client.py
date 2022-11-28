83362c130dfdeb8543c20f880a0b003d84d8cb01
Diego <poncelas91@gmail.com>
2022-11-28 18:00:35 +0100

    Added instagram app

---
+++ instagram-app/client.py
@@ -0,0 +1,168 @@
+"""
+See more functionality examples: https://instaloader.github.io/codesnippets.html
+
+"""
+
+import numpy as np
+import instaloader
+import heapq
+from datetime import datetime
+from getpass import getpass
+from itertools import takewhile
+from typing import Optional, List, Dict
+
+
+class InstagramClient:
+    """
+    A client to connect to Instagram and perform different operations using instaloader API.
+    It requires a username to log in.
+    """
+    def __init__(self, username: str, session_file: Optional[str] = None):
+        self.username = username
+        self.session_file = session_file
+        self._loader = instaloader.Instaloader()  # Instance of Instaloader class
+        self._context = self._loader.context      # low-level communication with Instagram
+        self._profile = instaloader.Profile.from_username(self._context, username=username)
+
+    def login(self) -> None:
+        if self.session_file:
+            try:
+                self._login_with_sessionfile(self.session_file)
+            except FileNotFoundError:
+                print("Session file not found. Login with password...")
+                self._login_with_password()
+        else:
+            self._login_with_password()
+
+    def _login_with_password(self) -> None:
+        """
+        Log in to instagram with username and password. Not to be called directly, use login() instead.
+        Raises:
+            InvalidArgumentException – If the provided username does not exist.
+            BadCredentialsException – If the provided password is wrong.
+            ConnectionException – If connection to Instagram failed.
+        """
+        print(f"Logged in instagram user {self.username} with password.")
+        password = getpass(prompt='Type your password: ')
+        try:
+            self._loader.login(user=self.username, passwd=password)
+        except instaloader.TwoFactorAuthRequiredException:
+            auth_code = input("2FA enabled. Please, type your authentication code: ")
+            self._loader.two_factor_login(auth_code)
+
+    def _login_with_sessionfile(self, filename: str) -> None:
+        """
+        Log in to Instagram from a previously stored session. Not to be called directly, use login() instead.
+        """
+        print(f"Logged in instagram user {self.username} with session file.")
+        self._loader.load_session_from_file(username=self.username, filename=filename)
+
+    def close_session(self) -> None:
+        """ Close instagram session"""
+        self._loader.close()
+
+    @property
+    def followers(self):
+        """ Return username followers """
+        return np.array([f.username for f in self._profile.get_followers()])
+
+    @property
+    def followees(self):
+        """ Return username followees """
+        return np.array([f.username for f in self._profile.get_followees()])
+
+    def get_difference_followers_followees(self, ignore: Optional[List[str]] = None) -> List[str]:
+        """
+        Return the difference between user followers and followees.
+        :param ignore -> path to file with accounts to be ignored when comparing
+        """
+        difference_followers_followees = np.setdiff1d(self.followees, self.followers)
+        if not ignore:
+            return [user for user in difference_followers_followees]
+        return [user for user in difference_followers_followees if user not in ignore]
+
+    def get_top_liked_posts(self, max_posts: int) -> Dict:
+        """
+        Return a Dict with the number of likes and post url for the top n liked post.
+        :param max_posts: maximum number of posts to return
+        """
+        top_n_likes = {}
+        posts = {f.shortcode: f.likes for f in self._profile.get_posts()}
+        # Order likes from top to bottom
+        lst = heapq.nlargest(max_posts, [like for shortcode, like in posts.items()])
+        sorted_dict = sorted(posts.items(), key=lambda item: item[1], reverse=True)
+        # Save and return results
+        for element in [(key, value) for key, value in sorted_dict if value in lst]:
+            top_n_likes[element[0]] = element[1]
+        return top_n_likes
+
+    def download_user_posts(self, start_date: str, end_date: str, username: Optional[str] = None,
+                            download_video: bool = True) -> None:
+        """
+        Download posts from the logged/given user (if possible).
+        :param username: an alternative username
+        :param start_date: initial date to download posts
+        :param end_date: end date to download posts
+        :param download_video: if True, download also videos
+        """
+        start_date = self._convert_str_datetime(start_date)
+        end_date = self._convert_str_datetime(end_date)
+
+        try:
+            if not username:
+                username = self.username
+                profile = self._profile
+            else:
+                profile = instaloader.Profile.from_username(self._context, username=username)
+            print(f"Downloading posts from user {username}...")
+            try:
+                # takewhile selects values from iterator that fulfill a given condition
+                for post in takewhile(lambda p: start_date <= p.date <= end_date, profile.get_posts()):
+                    if download_video:
+                        self._loader.download_post(post, username)
+                    if not post.is_video:
+                        self._loader.download_post(post, username)
+            except instaloader.PrivateProfileNotFollowedException:
+                print(f"Error, you can't access user {username} profile.")
+        except Exception as e:
+            print(e)
+
+    def download_hashtag_posts(self, start_date: str, end_date: str, hashtag: str,
+                               download_video: bool = True) -> None:
+        """
+        Download posts based on a given hashtag
+        :param hashtag: any hashtag
+        :param start_date: initial date to download posts
+        :param end_date: end date to download posts
+        :param download_video: if True, download also videos
+        """
+        profile = instaloader.Hashtag.from_name(self._context, hashtag)
+        start_date = self._convert_str_datetime(start_date)
+        end_date = self._convert_str_datetime(end_date)
+
+        # See https://instaloader.github.io/codesnippets.html for detailed explanation of the following logic
+        k = 0  # initiate k
+        # k_list = []  # uncomment this to tune k
+
+        for post in profile.get_posts():
+            postdate = post.date
+            if postdate > end_date:
+                continue
+            elif postdate <= start_date:
+                k += 1
+                if k == 50:
+                    break
+                else:
+                    continue
+            else:
+                if download_video:
+                    self._loader.download_post(post, hashtag)
+                if not post.is_video:
+                    self._loader.download_post(post, hashtag)
+
+    @staticmethod
+    def _convert_str_datetime(date: str) -> datetime:
+        try:
+            return datetime.strptime(date, '%Y/%m/%d')
+        except ValueError as e:
+            print(e)
---
+++ instagram-app/main.py
@@ -0,0 +1,145 @@
+"""
+Main file to execute instagram-app (console app).
+
+SETUP:
+    -SESSION_FILE -> Path to session file. Leave blank in order to login with user password
+    -IGNORE_FILE  -> Path to file with usernames to ignore in Difference followers-followees (optional)
+    -MAX_ERRORS   -> Define max number of input errors before exiting the application
+
+
+TODO - implement asyncio
+TODO - function to save output
+
+
+OLD CONCURRENT IMPLEMENTATION
+def execution(user_session, likes):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future1 = executor.submit(user_session.get_top_n_posts, likes)
+        future2 = executor.submit(user_session.get_difference_followers_followees)
+        future3 = executor.submit(session_login.download_posts, "2021, 01, 01", "all")
+
+        print(f"Finding Top {likes} liked posts for {session.username}...")
+        for k, v in future1.result().items():
+            print(f"Post url: https://www.instagram.com/p/{k}/    Likes: {v}")
+        print("\n")
+
+        print("Finding people that does not follow you back...")
+        if type(future2.result()) == str:
+            print(future2.result())
+        else:
+            print(*future2.result())
+"""
+
+import sys
+from typing import Union
+from client import InstagramClient
+
+
+# SETUP BEFORE EXECUTION
+SESSION_FILE: str = r'C:\Users\ponce\AppData\Local\Instaloader\session-diegop__03'
+IGNORE_FILE: str = r'resources/ignore.txt'
+MAX_ERRORS: int = 3
+
+
+def menu() -> int:
+    """ Display user menu and return user choice """
+    menu_error_counter = 0
+    print("Select an action: \n"
+          "1- Get the difference of followers-followees.\n"
+          "2- Find your most liked posts.\n"
+          "3- Download posts from your account.\n"
+          "4- Download posts from another user account.\n"
+          "5- Download posts from hashtag.\n"
+          "6- Close program.")
+    try:
+        if menu_error_counter == MAX_ERRORS:
+            print("Error, exiting application...")
+            raise sys.exit(1)
+        return int(input())
+    except ValueError:
+        print("Incorrect value. Please, enter an integer")
+        menu_error_counter += 1
+
+
+def get_download_input() -> Union[None, tuple[str, str, bool]]:
+    """ Get input to download posts """
+    _start_date = input("Type initial date in the format YYYY/MM/DD: ")
+    _end_date = input("Type final date in the format YYYY/MM/DD: ")
+
+    if not _end_date >= _start_date:
+        print("Final date must be greater or equal to initial date.")
+        return
+
+    _download_video = input("Download videos? Y/N: ").upper()
+
+    if _download_video in ["Y", "YES"]:
+        return _start_date, _end_date, True
+    elif _download_video in ["N", "NO"]:
+        return _start_date, _end_date, False
+    else:
+        print("Invalid options. Please type yes (Y) or no (N).\n")
+        return
+
+
+
+
+
+if __name__ == "__main__":
+    print("---- Instagram Application to analyze data ----\n")
+
+    username = input("Type your username: ")
+    session = InstagramClient(username, SESSION_FILE)
+    session.login()
+    print("Login successful.\n")
+
+    while True:
+        user_choice = menu()
+
+        if user_choice == 1:
+            print("Recovering users that are not following you back...")
+            if IGNORE_FILE:
+                ignore_accounts = [i.rstrip("\n") for i in open(IGNORE_FILE, 'r')]
+                result = session.get_difference_followers_followees(ignore_accounts)
+            else:
+                result = session.get_difference_followers_followees()
+            print(*result, sep='\n')
+
+        elif user_choice == 2:
+            try:
+                max_posts = int(input("Max number of posts to find: "))
+            except ValueError:
+                print("Incorrect value. Please, enter an integer.")
+                continue
+            print(f"Finding top {max_posts} liked posts for {session.username}...")
+            result = session.get_top_liked_posts(max_posts=max_posts)
+            for k, v in result.items():
+                print(f"Post url: https://www.instagram.com/p/{k}/    Likes: {v}")
+
+        elif user_choice in (3, 4, 5):
+            try:
+                start_date, end_date, download_video_flag = get_download_input()
+                if user_choice == 3:
+                    session.download_user_posts(start_date, end_date, download_video=download_video_flag)
+                if user_choice == 4:
+                    new_username = input("Type username target: ")
+                    session.download_user_posts(start_date, end_date, username=new_username,
+                                                download_video=download_video_flag)
+                if user_choice == 5:
+                    hashtag = input("Type hashtag to search: ")
+                    session.download_hashtag_posts(start_date, end_date, hashtag=hashtag,
+                                                   download_video=download_video_flag)
+            except ValueError as e:
+                print(f'{e}\n')
+                continue
+            except TypeError:
+                continue
+
+        elif user_choice == 6:
+            print("Session closed.")
+            session.close_session()
+            raise sys.exit(0)
+
+        else:
+            print("Invalid option. Please, try again.")
+
+        print()
