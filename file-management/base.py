eafe0e22b81a045767d5900a2810c65bb2dc3460
Diego <poncelas91@gmail.com>
2022-12-11 17:18:41 +0100

    some corrections for base.py and renamer.py

--- file-management/base.py
+++ file-management/base.py
@@ -3,10 +3,11 @@ Class Files defines all files in a given path, filtered by file extension and a 
 Hidden files can be selected or not.
 """
 
+import sys
 import csv
 import PyPDF2
 from pathlib import Path
-from typing import Tuple, List
+from typing import Tuple, List, Union
 
 
 # Valid filetype filters
@@ -33,7 +34,7 @@ class Files:
     def __str__(self) -> str:
         return f"Object that represents files at {self.path}\n" \
                f"View hidden files set to {self.hidden_files}\n" \
-               f"Keyword filter: {self.keyword_filter}\n"
+               f"Keyword filter: {self.keyword_filter}.\n"
 
     def __getitem__(self, keyword) -> List[str]:
         """
@@ -46,36 +47,35 @@ class Files:
         """ Return a list of filenames in the directory. """
         return self._files
 
-    def read_document(self, file: Path, enconding: str = 'utf8') -> str:
+    def read_document(self, file: Path, enconding: str = 'utf8') -> Union[str, Tuple[bool, str]]:
         """
         Read document files (files that are in FILE_EXTENSION['DOCUMENT']).
         :param file: filepath as Path object.
-        :param enconding: sets an enconding for .txt or .csv files.
+        :param enconding: enconding for .txt or .csv files.
         """
-        if file.name in self._files:
-            if file.suffix in FILE_EXTENSIONS['DOCUMENT']:
-                try:
-                    if file.suffix == '.txt':
-                        with open(file, 'r', encoding=enconding) as f:
-                            return f.read()
-                    elif file.suffix == '.csv':
-                        with open(file, 'r', encoding=enconding) as f:
-                            return '\n'.join(str(row) for row in csv.reader(f))
-                    elif file.suffix == '.pdf':
-                        with open(file, 'rb') as f:
-                            pdf_obj = PyPDF2.PdfFileReader(f)
-                            return '\n'.join(
-                                [pdf_obj.getPage(page).extractText() for page in range(0, pdf_obj.numPages)])
-                    else:
-                        return "File extension currently not supported."
-                except PermissionError:
-                    print(f"Permission denied: can't open file {file}")
-                except UnicodeDecodeError:
-                    print("Encode error. Try different encoding.")
+        if file.name not in self._files:
+            return False, "File not found."
+
+        if file.suffix not in FILE_EXTENSIONS['DOCUMENT']:
+            return False, f"File is not of type {FILE_EXTENSIONS['DOCUMENT']}."
+
+        try:
+            if file.suffix == '.txt':
+                with open(file, 'r', encoding=enconding) as f:
+                    return f.read()
+            elif file.suffix == '.csv':
+                with open(file, 'r', encoding=enconding) as f:
+                    return '\n'.join(str(row) for row in csv.reader(f))
+            elif file.suffix == '.pdf':
+                with open(file, 'rb') as f:
+                    pdf_obj = PyPDF2.PdfFileReader(f)
+                    return '\n'.join([pdf_obj.getPage(page).extractText() for page in range(0, pdf_obj.numPages)])
             else:
-                return f"File is not of type {FILE_EXTENSIONS['DOCUMENT']}"
-        else:
-            return "File not found."
+                return False, "File extension currently not supported."
+        except PermissionError:
+            print(f"Permission denied: can't open file {file}")
+        except UnicodeDecodeError:
+            print("Encode error. Try different encoding.")
 
     def _get_filepaths(self) -> List[Path]:
         """ Return a list of all the filepaths in the directory. """
@@ -89,3 +89,37 @@ class Files:
                     self.keyword_filter in path.stem.lower():
                 paths.append(path)
         return paths
+
+
+
+def get_user_input() -> Tuple[str, str, str, bool]:
+    """ Return user input. """
+    try:
+        path = input("Root path: ")
+        file_ext = input("Select file extensions from AUDIO/PICTURE/VIDEO/DOCUMENT/ALL: ").upper()
+
+        if not file_ext:
+            file_ext = 'ALL'
+        print(f"{file_ext} selected")
+
+        keyword_filter = input("Filter file-management by keyword [leave it blank to skip]: ").lower()  # NOT CASE SENSITIVE
+        hidden_files = input("Show hidden file-management? Y/N: ").upper()
+        print()
+
+        if hidden_files in ("Y", "YES"):
+            return path, file_ext, keyword_filter, True
+        elif hidden_files in ("N", "NO"):
+            return path, file_ext, keyword_filter, False
+        else:
+            print("ERROR - Invalid input given.")
+            raise sys.exit(1)
+
+    except KeyError:
+        print("ERROR - Incorrect file type selected.")
+        print("Valid options: AUDIO, PICTURE, VIDEO, DOCUMENT, ALL.")
+        raise sys.exit(1)
+
+    except FileNotFoundError:
+        print("ERROR - Incorrect path selected.")
+        print("Please, provide a valid directory.")
+        raise sys.exit(1)
---
+++ file-management/renamer.py
@@ -0,0 +1,106 @@
+"""
+A simple program to rename file-management sequentially, using predetermined formats
+"""
+
+import sys
+import base
+from pathlib import Path
+from typing import Optional
+
+
+class FileRenamer(base.Files):
+    def rename_files(self, pattern: str, text_to_remove: Optional[str] = None) -> None:
+        """ Rename a group of file-management using a pattern. """
+        _files = self._get_filepaths()
+
+        for file in _files:
+            if text_to_remove:
+                new_name = self._replace_filename(file.name, pattern, text_to_remove)
+            else:
+                new_name = self._replace_filename(file.name, pattern)
+
+            if _files.index(file) == 0:
+                print(f"Files will be renamed in the following format: {new_name}")
+                _continue = input("Continue? -> Y/N\n").upper()
+                if _continue in ('Y', 'YES'):
+                    pass
+                elif _continue == ('N', 'NO'):
+                    print("Process cancelled")
+                    break
+                else:
+                    print("Invalid option. Process stopped")
+                    break
+            file.rename(Path(f'{file.parents[0]}\\{new_name}'))
+            print(f"{file.name} renamed to {new_name}")
+
+    @staticmethod
+    def _replace_filename(filename: str, pattern: str, text_to_remove: Optional[str] = None) -> str:
+        """
+        Return new filename for a given pattern.
+        Current supported pattern:
+            -REMOVE_WHITESPACES -> remove whitespaces
+            -REMOVE_TEXT -> remove a given string
+            -OPPO_PICTURE -> remame Oppo phone photos in the format YYYYMMDD_hhmmss
+            -OPPO_VIDEO -> remame Oppo phone videos in the format VID_YYYYMMDD_hhmmss
+            -LG_PICTURE -> remame LG phone photos in the format YYYYMMDD_hhmmss
+            -LG_VIDEO -> remame LG phone videos in the format VID_YYYYMMDD_hhmmss
+        """
+        if pattern in ('REMOVE_WHITESPACES', '1'):
+            return f"{filename.replace(' ', '')}"
+        elif pattern in ('REMOVE_TEXT', '2'):
+            return f"{filename.replace(text_to_remove, '')}"
+        elif pattern in ('OPPO_PICTURE', '3'):
+            return f"{filename[3:11]}_{filename[11:]}"
+        elif pattern in ('OPPO_VIDEO', '4'):
+            return f"{filename[0:3]}_{filename[3:11]}_{filename[11:]}"
+        elif pattern in ('LG_PICTURE', '5'):
+            return f"{filename[4:12]}_{filename[13:19]}.jpg"
+        elif pattern in ('LG_VIDEO', '6'):
+            return f"{filename[0:19]}.mp4"
+        else:
+            print("ERROR - Invalid pattern.")
+            raise sys.exit(1)
+
+    @staticmethod
+    def select_pattern() -> str:
+        """ Return rename pattern selected by the user. """
+        print("Select pattern to rename file-management: \n"
+              "1) REMOVE_WHITESPACES -> remove whitespaces from filename\n"
+              "2) REMOVE_TEXT        -> remove a given text from filename\n"
+              "3) OPPO_PICTURE       -> remame Oppo phone photos in the format YYYYMMDD_hhmmss\n"
+              "4) OPPO_VIDEO         -> remame Oppo phone videos in the format VID_YYYYMMDD_hhmmss\n"
+              "5) LG_PICTURE         -> remame LG phone photos in the format YYYYMMDD_hhmmss\n"
+              "6) LG_VIDEO           -> remame LG phone videos in the format VID_YYYYMMDD_hhmmss")
+        return input().upper()
+
+
+
+
+if __name__ == "__main__":
+    print("---- Script to rename file-management ----\n")
+    try:
+        user_path, user_file_ext, user_keyword, user_hidden_files = base.get_user_input()
+
+        F = FileRenamer(path=Path(user_path),
+                        file_ext=base.FILE_EXTENSIONS[user_file_ext],
+                        keyword_filter=user_keyword,
+                        hidden_files=user_hidden_files)
+        print(f"Found the following files at {user_path}:")
+        print(*F.list_files(), sep='\n')
+        print()
+
+        if not F.list_files():
+            print(f"No files found at {user_path}")
+            raise sys.exit(0)
+
+        rename_pattern = F.select_pattern()
+
+        if rename_pattern in ('REMOVE_TEXT', '2'):
+            user_text_to_remove = input("Type text to remove: ")  # case sensitive
+            F.rename_files(rename_pattern, user_text_to_remove)
+        else:
+            F.rename_files(rename_pattern)
+
+    except KeyboardInterrupt:
+        print("Program interrupted by user.")
+        raise sys.exit(1)
